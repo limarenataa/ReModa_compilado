3a71e5d156f7f4d9ac8ea7ffa2df03156119193c
Renata Lima <limarenataa@gmail.com>
2020-10-14 00:04:11 -0300

    projeto com funções em js

---
+++ projeto-js/funcoes.js
@@ -0,0 +1,42 @@
+
+//Filtrar os produtos por categoria 
+function exibir_categoria(categoria) {
+
+    let elementos = document.getElementsByClassName('box-produtos');
+    console.log(elementos);
+
+    for (let i = 0; i < elementos.length; i++) {
+        console.log(elementos[i].id);
+        if (categoria == elementos[i].id) {
+            elementos[i].style = "inline-block"; //tive que alterar pq só block não tava indo
+        } else {
+            elementos[i].style = "display:none";
+        }
+    }
+};
+
+//Exibir todos os produtos
+let exibir_todas = () => {
+    let elementos = document.getElementsByClassName('box-produtos');
+
+    for (let i = 0; i < elementos.length; i++) {
+        elementos[i].style = "block";
+    }
+};
+
+//Expandir o tamanho da imagem 
+let destaqueImg = (imagem) => {
+    console.log(imagem);
+    if (imagem.width == 180 && imagem.height == 180) {
+        imagem.width = 280
+        imagem.height = 280
+        
+    }else {
+        imagem.width = 180
+        imagem.height = 180
+    }
+}
+
+
+
+    
