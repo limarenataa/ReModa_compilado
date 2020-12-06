a4afa5731ab898998fc0271771438cd6ddd8bd48
Renata Lima <limarenataa@gmail.com>
2020-12-05 21:07:57 -0300

    Refatorando a página TodasLojas

--- projeto-react/src/Pages/TodasLojas.js
+++ projeto-react/src/Pages/TodasLojas.js
@@ -1,29 +1,11 @@
-import { Container, ListGroup } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import Lojas from '../Components/Lojas'
 
 
 function TodasLojas() {
     return (
         <Container>
-            <ListGroup>
-                <ListGroup.Item>
-                    <h4>Rio de Janeiro</h4>
-                    <p>Avenida das Américas 000</p>
-                    <p>Barra da Tijuca</p>
-                    <p>(21)0000-0000</p>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <h4>São Paulo</h4>
-                    <p>Avenida São Paulo 000</p>
-                    <p>Jardins</p>
-                    <p>(11)0000-0000</p>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <h4>Salvador</h4>
-                    <p>Avenida Salvador 000</p>
-                    <p>Alphaville</p>
-                    <p>(71)0000-0000</p>
-                </ListGroup.Item>
-            </ListGroup>
+           <Lojas />
         </Container>
         );
 };
