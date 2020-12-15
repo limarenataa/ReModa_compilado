d8237295fd2fa241e568a2d6e4b578b54b583a6d
Renata Lima <limarenataa@gmail.com>
2020-12-15 16:08:53 -0300

    modificando a conexão com o bd

--- projeto-bootstrap/conexao.php
+++ projeto-bootstrap/conexao.php
@@ -2,7 +2,7 @@
         $servername = "localhost";
         $username = "root";
         $password = "";
-        $database = "remoda";
+        $database = "remodaphp";
 
 
         $conn = mysqli_connect($servername, $username, $password, $database);
