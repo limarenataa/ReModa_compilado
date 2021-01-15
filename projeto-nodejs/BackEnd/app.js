ca7ad6cc9aecb3cb483d5cc4df286cc7cd27a304
Renata Lima <limarenataa@gmail.com>
2021-01-14 22:22:43 -0300

    Adicionando método POST no formulário da página Pedidos

--- projeto-nodejs/BackEnd/app.js
+++ projeto-nodejs/BackEnd/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require('cors')
+const port = "4000";
 
-app.use(cors())
+app.use(cors()) //se não tiver o cors, não funciona
+
+app.use(express.json()) 
+app.set('view engine', 'ejs');
 
 
 //Puxar dados do BD
@@ -17,21 +21,80 @@ app.get("/", (req, res, next) => {
     }); 
 
     connection.query("SELECT * FROM produtos",  (error, result)  => {
-        res.json({objetoDados:result}) //Pega os dados que vem do banco e responde em json (res=response)
+        res.json(result) //Pega os dados que vem do banco e responde em json (res=response)
+
+    })
+});
+
+
+app.get("/pedidos", (req, res, next) => {
+
+    const mysql = require('mysql')
+    const connection = mysql.createConnection({
+        host: 'localhost', 
+        user: 'root',
+        password: '',
+        database: 'remoda'
+    }); 
+
+    connection.query("SELECT * FROM clientes",  (error, result)  => {
+        res.json(result) //Pega os dados que vem do banco e responde em json (res=response)
 
     })
 });
 
 
 
-//Enviar dados para o BD
+//Enviando dados para o BD
+app.post('/pedidos', (req, res) => {
 
+    const mysql = require('mysql')
+    const connection = mysql.createConnection({
+        host: 'localhost', 
+        user: 'root',
+        password: '',
+        database: 'remoda'
+    }) ;
+
+
+    let dadosCliente = []
+    //joga os dados para dentro do array da variavel let dados
+    dadosCliente.push({
+
+        nomecliente: req.body.nomecliente,
+        email: req.body.email,
+        telefone: req.body.telefone,
+        endereco: req.body.endereco,
+        complemento: req.body.complemento
+    })
+
+    //colocando os dados recebidos dentro da nossa tabela
+    connection.query('INSERT INTO clientes SET ?', dadosCliente, () => {
+        dadosCliente = [] //zerando array
+        return res.json({message:"Dados enviados para tabela clientes com sucesso"})
+    })
+
+    let dadosPedido = []
+    dadosPedido.push({
+
+        descricao_produto: req.body.descricao_produto,
+        preco: req.body.preco,
+        quantidade: req.body.quantidade,
+        preco_final: req.body.preco_final
+    })
+
+    //colocando os dados recebidos dentro da nossa tabela
+    connection.query('INSERT INTO pedidos SET ?', dadosPedido, () => {
+        dadosPedido = [] //zerando array
+        return res.json({message:"Dados enviados para tabela pedidos com sucesso"})
+    })
+})
 
 
 
 //Criando servidor
-app.listen(4000, () => {
-  console.log("O servidor subiu na porta 4000");
+app.listen(port, () => {
+  console.log(`O servidor subiu na porta ${port}`);
 });
 
 
